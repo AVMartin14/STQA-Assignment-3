b029022cb28b208ffdceac8615040801d30875fb
Ashlyn Martin <ashlynmartin23@gmail.com>
2023-04-10 18:14:45 -0500

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: "node",
+    coverageDirectory: "./coverage",
+    collectCoverage: true,
+    coverageReporters: ["text", "lcov"],
+    testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"]
+  };
+  
