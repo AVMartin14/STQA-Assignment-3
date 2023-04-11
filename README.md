f1f2cf8afcc4c1bfc5026f6ee9c0767ac3637cd4
Ashlyn Martin <ashlynmartin23@gmail.com>
2023-04-10 19:12:22 -0500

    Coveralls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # STQA-Assignment-3
+
+Have to run "npm install coveralls --save-dev" to get the coveralls folder in the nodes_module.
+
+Have to do "npm run coveralls" to get coverage report.
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: "node",
-    coverageDirectory: "./coverage",
-    collectCoverage: true,
-    coverageReporters: ["text", "lcov"],
-    testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"]
-  };
-  
